ea97b3083d529cf8f1aafd6708c3b3210c177d55
ahmadsolehmihardi <ahmadsolehmihardi@gmail.com>
2026-04-02 16:37:56 +0700

    Add files via upload

---
+++ belajarArray.js
@@ -0,0 +1,60 @@
+let fruits = ["banana","orange","dragon","pinapel","rambutan"];
+let namen = ["ahmad"]
+let angka = [1,2,3,4,5];
+let namapanjang = "ahmad soleh mihardi"
+
+//push,pop,unshift,shift
+// fruits.push("manggo");
+// console.log(fruits);
+// fruits.unshift('durian');
+// console.log(fruits)
+// fruits.shift();
+// console.log(fruits)
+// fruits.pop();
+// console.log(fruits)
+
+// //looping
+// for(let i = 0; i < fruits.length; i++){
+//    console.log(fruits[i])
+// }
+
+// fruits.forEach((item)=>
+//     console.log(item)
+// )
+
+// fruits.map((item)=>
+// console.log(item)
+// )
+
+// //spreet array
+// let result = [...fruits].reverse();
+// console.log(result)
+
+//add 2 array
+// let result = fruits.concat(angka);
+// console.log(result)
+// let result2 = [...fruits,...angka];
+// console.log(result2)
+
+// split
+// let result = namapanjang.split('');
+// result.reverse();
+// console.log(result)
+
+//find dan filter
+// let result = angka.find(item=>item == 1);
+// console.log(result)
+// let result2 = angka.filter(item=>item > 1);
+// console.log(result2)
+
+//include -> mencocokan return false and true
+// let result = fruits.includes("banana");
+// console.log(result)
+
+//indexOf -> return menampilkan index berapa di sebuah array
+// let result = fruits.indexOf("orange");
+// console.log(result)
+
+//slice -> return hanya mengambil di index pertama dan di ambil sebelum end/terakhir
+// let result = fruits.slice(0,4);
+// console.log(result)
---
+++ pertanyaan interview.js
@@ -0,0 +1,117 @@
+// let str = "ahmad soleh mihardi";
+// let result = str.split(" ").map((value,key)=>
+//     key === 0
+//     ?value.split("").reverse().join(""):value
+// ).join(" ");
+// console.log(result);
+
+
+
+// let arr1 = [100,200,300];
+// let arr2 = [100,200,300];
+// let result = {...arr1,...arr2};
+// let result2 = [...arr1,...arr2].join(" ");
+// let nuul =  
+// console.log(result);
+// console.log(result2);
+// console.log(nuul);
+
+
+// let abc = "ahmad";
+// abc[2] = "d";
+// console.log(abc);
+// let a = 100 ;
+// let z = a++;
+// console.log(a+z);
+
+// let str = " my name ahmad";
+// // ym eman 
+// let result = str.split(" ").map((value)=>
+//     value.split("").reverse().join("")
+// ).join(" ")
+// console.log(result);
+
+// let arr = [1,10,18,14,6];
+
+// function abc(b,...a){
+//     console.log(a)
+
+// }
+// abc(8,9,10,11,12);
+
+// let arr2 = ["a",1,"b",2,"c",3];
+
+// let result2 = arr2.filter((val)=> typeof val == "string").join("");
+// let result3 = arr2.filter((val)=> typeof val == "number").join("");
+
+// let hasilresult2 = result2.split("").map((val)=> isNaN(val) == "String");
+// console.log(hasilresult2);
+// console.log(result3);
+
+
+// let x = {},y={name: "Ahmad"},z={name: "mihardi"}
+// x[y] = {name:"soleh"};
+// x[z] = {name:"dira"};
+// console.log(x[y]);
+
+// let arr = [...]; // undifine
+// let arr2 = [1,2,3]; // 
+// let result = [[1,2,3],[4,5,6]]; //(2) [Array(3), Array(3)]
+// console.log(result)
+
+// let arr = [1,18,9,81,98,124,78];
+// let result = [...arr.sort((a,b)=> a-b)] //dasarnya short default unt mengurutkan huruf, jika angka harus ada parameter
+// console.log(result)
+
+// let arr = [1,18,9,81,98,124,78];
+// let result = arr.map((item)=> console.log(item))
+
+// let arr = [1,18,9,81,98,124,78,78,81];
+// let result = arr.length=[]; // mengosongkan element array
+
+// let arr = [1,18,9,81,98,124,78,78,81];
+// let result = [...new Set(arr)] // menghapus element yang dobel
+// console.log(result)
+
+// function abc(a,b,c){}
+// function qpr(a=0,b,c){}
+// console.log(abc.length,qpr.length) // length method
+
+// console.log([] == []);
+// console.log([] === []);
+// console.log(NaN == NaN);
+// console.log(NaN === NaN);
+
+
+// let str = "my name ahmad soleh";
+// let result = str.split(' ').map((val)=>
+//     val.charAt(0).toUpperCase()+val.slice(1) //chartAt() untuk mendefinisikan karakter
+// ).join(' ')
+// console.log(result)
+
+// let a=50;
+// {
+//     let a= 500;
+// }
+// let b=a;
+// {
+//     let b= 5000;
+// }
+// console.log(a);
+
+// var a=50;
+// function abc(){
+//     var a= 500;
+// }
+// let b=a;
+// function abcd(){
+//     let b= 5000;
+// }
+// console.log(b);
+
+let arr = [1,4,5];
+[...arr].splice(1,1);
+console.log(arr)
+console.log("ahmad"-"2");
+
+
